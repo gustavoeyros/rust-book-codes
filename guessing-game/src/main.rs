ca91146d8031a2ef0f7f87cf0d93fb67112ef000
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-10-24 17:54:07 -0300

    feat: guessing game

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Adivinhe o número!");
+
+    let numero_secreto = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        print!("Digite o seu palpite.");
+
+        let mut palpite: String = String::new();
+
+        io::stdin()
+            .read_line(&mut palpite)
+            .expect("Falha ao ler entrada");
+
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Você disse: {}", palpite);
+
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                print!("Você acertou!");
+                break;
+            }
+        }
+    }
+}
